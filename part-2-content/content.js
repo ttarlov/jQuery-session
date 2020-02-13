3bf6dbd4004f9131595bb965373eb39377f0cda5
Taras Tarlov <ttarlov@gmail.com>
2020-02-13 14:00:03 -0700

    Finish part2 Exersises.

--- part-2-content/content.js
+++ part-2-content/content.js
@@ -1,14 +1,17 @@
-const $ = require('jquery');
-let btn = document.querySelector('button');
-btn.addEventListener('click', function(e){
-  e.preventDefault();
-})
-
-let clearBtn = document.querySelector('#clear-btn');
-clearBtn.addEventListener('click', function(){
-  document.querySelectorAll('input').forEach(input => {
-    input.value = "";
-  })
-})
-
-console.log($('p').text())
+// const $ = require('jquery');
+// let btn = document.querySelector('button');
+// btn.addEventListener('click', function(e){
+//   e.preventDefault();
+// })
+//
+// let clearBtn = document.querySelector('#clear-btn');
+// clearBtn.addEventListener('click', function(){
+//   document.querySelectorAll('input').forEach(input => {
+//     input.value = "";
+//   })
+// })
+$('button[type="submit"]').text('Add New Buddy')
+$('label[for="confirm"]').text('Yesssss!!!')
+$('label[for="deny"]').text('Nooooo!!!')
+$('input').val('default value')
+$('h1,h2').text('Contact Form')
